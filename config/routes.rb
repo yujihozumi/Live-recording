8fffed881fced5b4025031fbf6a881e6c24f0eea
yujihozumi <yuu.60011@gmail.com>
2022-02-28 15:22:09 +0900

    resources追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users,
     controllers: { registrations: 'registrations' }
-  root 'pages#home'
+  root 'posts#index'
   get '/users/:id', to: 'users#show', as: 'user'
+
+  resources :posts, only: %i(new create index show destroy) do
+    resources :photos, only: %i(create)
+    resources :likes, only: %i(create destroy)
+    resources :comments, only: %i(create destroy)
+  end
 end
