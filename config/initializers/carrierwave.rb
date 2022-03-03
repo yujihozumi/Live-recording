5572d970f2c5f7f2cbcf83f063e54c25bee424a4
yujihozumi <yuu.60011@gmail.com>
2022-03-03 15:34:12 +0900

    first commit

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,7 +1,11 @@
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
 unless Rails.env.development? || Rails.env.test?
   CarrierWave.configure do |config|
     config.fog_credentials = {
-      provider: 'AWS',
+      provider: 'fog/aws',
       aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
       aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
       region: ENV['AWS_DEFAULT_REGION']
